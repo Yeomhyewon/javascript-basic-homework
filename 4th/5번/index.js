aee578703abe0bf7e78d8b5e40c3a96b9fb29b2d
Yeomhyewon <duagpdnjs1@naver.com>
2023-11-14 14:12:07 +0900

    과제완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,34 @@
 // 여기에 1번 문제의 답을 작성하세요.
+// fetch("https://dummyjson.com/products")
+//   .then((res) => res.json())
+//   .then(console.log);
 
+async function getData() {
+  const response = await fetch("https://dummyjson.com/products");
+  const result = await response.json();
+  return result;
+}
 // 여기에 2번 문제의 답을 작성하세요.
+// fetch("https://dummyjson.com/products/add", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     title: "BMW Pencil",
+//     /* other product data */
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then(console.log);
+
+async function postData() {
+  const response = await fetch("https://dummyjson.com/products/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: "BMW Pencil",
+      /* other product data */
+    }),
+  });
+  const data = await response.json();
+  return data;
+}
